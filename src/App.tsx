08ba50ae45461cc7d3888c579ba34ec0f70ec2d7
abdallahsultan003 <abdallahsultan00@gmail.com>
2026-07-23 01:06:46 +0300

    implement upload widget

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,12 @@ import { useNotificationProvider } from "./components/refine-ui/notification/use
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
 import Dashboard from "@/pages/dashboard.tsx";
-import {BookOpen, Home} from "lucide-react";
+import {BookOpen, GraduationCap, Home} from "lucide-react";
 import {Layout} from "@/components/refine-ui/layout/layout.tsx";
 import SubjectsList from "@/pages/subjects/list.tsx";
 import SubjectsCreate from "@/pages/subjects/create.tsx";
+import ClassesList from "@/pages/classes/list.tsx";
+import ClassesCreate from "@/pages/classes/create.tsx";
 
 function App() {
   return (
@@ -45,6 +47,13 @@ function App() {
                       list: '/subjects',
                       create: '/subjects/create',
                       meta: { label: 'Subjects', icon: <BookOpen />}
+                  },
+                  {
+                      name: 'classes',
+                      list: '/classes',
+                      create: '/classes/create',
+                      show: '/classes/show/:id',
+                      meta: { label: 'Classes', icon: <GraduationCap />}
                   }
 
               ]}
@@ -61,6 +70,11 @@ function App() {
                       <Route path="subjects">
                           <Route index element={<SubjectsList />} />
                           <Route path="create" element={<SubjectsCreate />} />
+                      </Route>
+
+                      <Route path="classes">
+                          <Route index element={<ClassesList />} />
+                          <Route path="create" element={<ClassesCreate />} />
                       </Route>
                   </Route>
               </Routes>
